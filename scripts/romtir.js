6d9e10ba8c5fd26ec61d4372da6c739df4e7f0c5
MasterNo2 <60903909+MasterNo2@users.noreply.github.com>
2020-12-13 10:36:04 -0600

    Create romtir.js

---
+++ scripts/romtir.js
@@ -0,0 +1,28 @@
+Events.on(ClientLoadEvent, () => {
+    const Romtir = new Planet("Romtir", Planets.sun, 1, 1);
+    Romtir.generator = new SerpuloPlanetGenerator();
+    Romtir.mesh = new SunMesh(
+      Starion, 7,
+      1, 0.7, 1.8, 0.7,1,
+      2.9,
+      
+    //detalles,none,none,none,none,none,brillo
+      Color.valueOf("46a6abFF"),
+      Color.valueOf("4ed7deFF"),
+      Color.valueOf("3bb9bfFF"),
+      Color.valueOf("1b7175"),
+      Color.valueOf("3b7487"),
+      Color.valueOf("74c4cf")
+    );
+    Romtir.orbitRadius = 6;
+    Romtir.orbitTime = 30;
+    Romtir.rotateTime = 20;
+    Romtir.bloom = true;
+    Romtir.radius = 2;
+    Romtir.accessible = false;
+    Romtir.hasAtmosphere = true;
+    Romtir.atmosphereColor = Color.valueOf("0b4e57");
+    Romtir.atmosphereRadIn = 0.02;
+    Romtir.atmosphereRadOut = 0.01;
+    Romtir.localizedName = "Romtir";
+}); 
