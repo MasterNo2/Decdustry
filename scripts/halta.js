ba6c348b5c7929ea75871867d26f46fe273a6ce5
MasterNo2 <60903909+MasterNo2@users.noreply.github.com>
2020-12-13 12:15:24 -0600

    Update halta.js

--- scripts/halta.js
+++ scripts/halta.js
@@ -1,7 +1,19 @@
 Events.on(ClientLoadEvent, () => {
     const halta = new Planet("halta", Planets.sun, 3, 1.2;
     halta.generator = new SerpuloPlanetGenerator();
-    halta.mesh = new HexMesh(sarpa, 2);
+    halta.mesh = new hexMesh(
+      halta, 7,
+      1, 0.7, 1.8, 0.7,1,
+      2.9,
+      
+    //detalles,none,none,none,none,none,brillo
+      Color.valueOf("e9f0f0"),
+      Color.valueOf("dfe6e6"),
+      Color.valueOf("ced9d9"),
+      Color.valueOf("d5e3e3"),
+      Color.valueOf("b7c9c9"),
+      Color.valueOf("c1e3e3")
+    );
     halta.orbitRadius = 4.75;
     halta.orbitTime = 1075
     halta.rotateTime = 360;
@@ -9,7 +21,8 @@ Events.on(ClientLoadEvent, () => {
     halta.radius = 0.87;
     halta.accessible = false;
     halta.hasAtmosphere = true;
-    halta.atmosphereColor = Color.valueOf("caeded");
+    halta.atmosphereColor = Color.valueOf("ccfcfc");
     halta.atmosphereRadIn = 0.82;
     halta.atmosphereRadOut = 0.825;
-    halta.localizedName = "halta";
+    halta.localizedName = "Halta";
+    });
