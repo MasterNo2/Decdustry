615297ca575ce5544f6749aa86e4049aa39ca489
MasterNo2 <60903909+MasterNo2@users.noreply.github.com>
2020-12-13 12:07:49 -0600

    Update halta.js

--- scripts/halta.js
+++ scripts/halta.js
@@ -1,17 +1,7 @@
 Events.on(ClientLoadEvent, () => {
     const halta = new Planet("halta", Planets.sun, 3, 1.8);
     halta.generator = new SerpuloPlanetGenerator();
-    halta.mesh = new HexMesh(
-        
-      halta, 2;
-        
-      Color.valueOf("e9f2f0"),
-      Color.valueOf("d5dbda"),
-      Color.valueOf("bbc7c5"),
-      Color.valueOf("cce0dd"),
-      Color.valueOf("d0f5ef"),
-      Color.valueOf("f5ffff")
-    );
+    halta.mesh = new HexMesh(halta, 2);
     halta.orbitRadius = 4.75;
     halta.orbitTime = 1075
     halta.rotateTime = 360;
